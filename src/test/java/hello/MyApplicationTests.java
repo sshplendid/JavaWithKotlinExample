c008dcab2366b8aea5279ba0cac9e24067176f7f
SeHun Shin <s.shin@marathoner.kr>
2019-01-17 00:14:58 +0900

    application test

--- src/test/java/hello/MyApplicationTests.java
+++ src/test/java/hello/MyApplicationTests.java
@@ -1,12 +1,22 @@
 package hello;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class MyApplicationTests {
     @Test
     public void testHello() {
         String[] args = {};
         HelloApplication app = new HelloApplication();
         app.main(args);
+    }
+
+    @Test
+    public void contextLoads() {
+
     }
 }
