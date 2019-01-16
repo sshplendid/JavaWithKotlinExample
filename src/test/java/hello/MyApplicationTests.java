66ab88e6d009e233732c0b705e15b92d1c0223de
SeHun Shin <s.shin@marathoner.kr>
2019-01-16 23:56:52 +0900

    spring app 클래스 추가

--- src/test/java/hello/MyApplicationTests.java
+++ src/test/java/hello/MyApplicationTests.java
@@ -1,2 +1,12 @@
+package hello;
+
+import org.junit.Test;
+
 public class MyApplicationTests {
+    @Test
+    public void testHello() {
+        String[] args = {};
+        HelloApplication app = new HelloApplication();
+        app.main(args);
+    }
 }
