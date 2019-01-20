1b06c109027006531debd8c60455efa8778a4c9a
SeHun Shin <s.shin@marathoner.kr>
2019-01-20 21:35:06 +0900

    data class: 비구조화 할당

--- src/test/kotlin/comparision/StudentKtVOTests.kt
+++ src/test/kotlin/comparision/StudentKtVOTests.kt
@@ -3,10 +3,10 @@ package comparision
 import org.junit.Assert.*
 import org.junit.Test
 
-class StudentKtVOTest {
+class StudentKtVOTests {
     @Test
     fun testToString() {
-        val obj: StudentKtVO = StudentKtVO(1, "Shawn", "shawn@gmail.com")
+        val obj: StudentKtVO = StudentKtVO(1, "Shawn", "shawn@email.me")
         val foo = KotlinUser(1)
 
         println("data class -> ${obj.toString()}")
@@ -26,6 +26,19 @@ class StudentKtVOTest {
 
         println("normal class -> ${foz == baz} ${foz === baz}")
         assertEquals(foo, bar)
-        assertEquals(foz, baz)
+        assertNotEquals(foz, baz)
+    }
+
+    @Test
+    fun testDestructinAssignments() {
+        val obj: StudentKtVO = StudentKtVO(1, "Shawn", "shawn@email.me")
+        // val foo = KotlinUser(1)
+
+        val (id, name, email) = obj; // 주 생성자 파라미터 순서대로 할당o
+        //val (bar) = foo
+
+        assertEquals(1, id)
+        assertEquals("Shawn", name)
+        assertEquals("shawn@email.me", email)
     }
 }
